3e0ba3594e57bb18d733844864bd7a23e00bf528
acse-sm321 <shuheng.mo21@imperial.ac.uk>
2022-06-18 22:59:18 +0100

    Implemented Context

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"myGin"
 	"net/http"
 )
@@ -59,15 +58,34 @@ func main() {
 
 	r := myGin.New()
 	// register the methods and then run
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	r.GET("/", func(c *myGin.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello myGin</h1>")
 	})
 
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.GET("/hello", func(c *myGin.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
+
+	// now implement context for Query + PostForm
+	// Context: create HTTP response effectively, clean up after HTTP request
+	r.POST("/login", func(c *myGin.Context) {
+		c.JSON(http.StatusOK, myGin.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
+	})
+
+	// Assume no context and we need to return a JSON, here is what we need to do:
+	//obj = map[string]interface{}{
+	//	"name":     "shuheng mo",
+	//	"password": "123456",
+	//}
+	//w.Header().Set("Content-Type", "application/json") // head info
+	//w.WriteHeader(http.StatusOK)                       // status code
+	//encoder := json.NewEncoder(w)                      // create json encoder
+	//if err := encoder.Encode(obj); err != nil {
+	//	http.Error(w, err.Error(), 500)
+	//}
 
 	r.Run(":9999")
 }
